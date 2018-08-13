42cfa60341c3db90afe106b53ec96c9d168abecc
bridger <bdysonsmith@gmail.com>
2018-08-13 14:26:47 -0400

    Move package installs into conditionals

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -4,4 +4,30 @@
 sudo yum -q update
 
 # sudo and add some packages
-sudo yum install java-1.8.0-openjdk-devel tmux zip unzip
+if [ -e /usr/bin/java ]; then
+	echo "** java is installed **"
+else
+	echo "** installing java **"
+	sudo yum install -y java-1.8.0-openjdk-devel
+fi
+
+if [ -e /usr/bin/tmux ]; then
+	echo "** tmux is installed **"
+else
+	echo "** installing tmux **"
+	sudo yum install -y tmux
+fi
+
+if [ -e /usr/bin/zip ]; then
+	echo "** zip is installed **"
+else
+	echo "** installing zip **"
+	sudo yum install -y zip
+fi
+
+if [ -e /usr/bin/unzip ]; then
+	echo "** unzip is installed **"
+else
+	echo "** installing unzip **"
+	sudo yum install -y unzip
+fi
