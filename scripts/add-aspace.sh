ef48250182976ce895f16628a8d9449d6ac07f11
bridger <bdysonsmith@gmail.com>
2018-08-13 12:16:46 -0400

    Curl and unzip archivesspace ZIP archive

--- scripts/add-aspace.sh
+++ scripts/add-aspace.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-# install archivesspace
-wget
+# acquire archivesspace
+sudo curl -L -O https://github.com/archivesspace/archivesspace/releases/download/v2.5.0/archivesspace-v2.5.0.zip
+
+# unpack the zip in /vagrant
+sudo unzip -d /vagrant/ archivesspace-v2.5.0.zip
+
