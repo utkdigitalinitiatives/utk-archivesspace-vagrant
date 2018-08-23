2b1b0921cb29063c68dbbef5f93bdb479115f741
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2018-08-23 14:56:26 -0400

    added a number of packages
    added a number of packages for python installation

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -1,33 +1,43 @@
 #!/usr/bin/env bash
 
+function evalyum {
+	local program_name=$1
+        local package_name=$2
+	if [ -e /usr/bin/{$1} ]; then
+		echo "** ${program_name} is installed **"
+	else
+		echo "** installing ${program_name} **"
+		echo `sudo yum install -q -y ${package_name}`
+	fi
+}
+
 # sudo and update the installed packages
-sudo yum -q update
+sudo yum -q -y update
 
-# sudo and add some packages
-if [ -e /usr/bin/java ]; then
-	echo "** java is installed **"
-else
-	echo "** installing java **"
-	sudo yum install -y java-1.8.0-openjdk-devel
-fi
+evalyum java java-1.8.0-openjdk-devel
+evalyum tmux tmux
+evalyum zip zip
+evalyum unzip unzip
+evalyum zlib-d zlib-devel
+evalyum bzip2 bzip2
+evalyum bzip2-d bzip2-devel
+evalyum git git
+evalyum readline-d readline-devel
+evalyum sqlite sqlite
+evalyum sqlite-d sqlite-devel 
+evalyum openssl-d openssl-devel
+evalyum xz xz
+evalyum xz-d xz-devel
+evalyum repomanage yum-utils
+evalyum grpi groupinstall
+evalyum devl development
+evalyum python3.6 https://centos7.iuscommunity.org/ius-release.rpm
+evalyum python3.6 python36u
+evalyum pip3.6 python36u-pip
 
-if [ -e /usr/bin/tmux ]; then
-	echo "** tmux is installed **"
-else
-	echo "** installing tmux **"
-	sudo yum install -y tmux
-fi
+#sudo mkdir /home/vagrant/bin
+#chown -R /home/vagrant/bin vagrant
+#curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer > /home/vagrant/bin/pyenv-installer
+#chmod 755 /home/vagrant/bin/pyenv-installer
+#exec /home/vagrant/bin/pyenv-installer
 
-if [ -e /usr/bin/zip ]; then
-	echo "** zip is installed **"
-else
-	echo "** installing zip **"
-	sudo yum install -y zip
-fi
-
-if [ -e /usr/bin/unzip ]; then
-	echo "** unzip is installed **"
-else
-	echo "** installing unzip **"
-	sudo yum install -y unzip
-fi
