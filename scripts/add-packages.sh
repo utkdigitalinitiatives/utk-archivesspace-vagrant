f214a1202a73c01fb225b9852700a840046bf3f9
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2018-08-27 17:04:43 -0400

    Fixing development group
    I was wrongly attempting to install the development group of programs. fixed hopefully.

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -1,39 +1,41 @@
 #!/usr/bin/env bash
 
+DI_LOG_DIR=/tmp
+DI_LOG_FILE=DI_as_packages.install.log
+
 function evalyum {
-	local program_name=$1
-        local package_name=$2
-	if [ -e /usr/bin/{$1} ]; then
-		echo "** ${program_name} is installed **"
-	else
-		echo "** installing ${program_name} **"
+	local package_name=$1
+	if ! (sudo yum info -q ${package_name} 2>&1 | grep -qP 'Repo\s+:\s+installed'  >> ${DI_LOG_DIR}/${DI_LOG_FILE} 2>&1); then
+		echo "** installing ${package_name} **"
 		echo `sudo yum install -q -y ${package_name}`
+	else
+		echo "** ${package_name} is installed **"
 	fi
 }
 
 # sudo and update the installed packages
-sudo yum -q -y update
+sudo yum -q -y update >> ${DI_LOG_DIR}/${DI_LOG_FILE} 2>&1
 
-evalyum java java-1.8.0-openjdk-devel
-evalyum tmux tmux
-evalyum zip zip
-evalyum unzip unzip
-evalyum zlib-d zlib-devel
-evalyum bzip2 bzip2
-evalyum bzip2-d bzip2-devel
-evalyum git git
-evalyum readline-d readline-devel
-evalyum sqlite sqlite
-evalyum sqlite-d sqlite-devel 
-evalyum openssl-d openssl-devel
-evalyum xz xz
-evalyum xz-d xz-devel
-evalyum repomanage yum-utils
-evalyum grpi groupinstall
-evalyum devl development
-evalyum python3.6 https://centos7.iuscommunity.org/ius-release.rpm
-evalyum python3.6 python36u
-evalyum pip3.6 python36u-pip
+evalyum java-1.8.0-openjdk-devel
+evalyum tmux
+evalyum zip
+evalyum unzip
+evalyum zlib-devel
+evalyum bzip2
+evalyum bzip2-devel
+evalyum git
+evalyum readline-devel
+evalyum sqlite
+evalyum sqlite-devel 
+evalyum openssl-devel
+evalyum xz
+evalyum xz-devel
+evalyum yum-utils
+evalyum https://centos7.iuscommunity.org/ius-release.rpm
+evalyum python36u
+evalyum python36u-pip
+
+`sudo yum install -q -y @"Development Tools" >> ${DI_LOG_DIR}/${DI_LOG_FILE} 2>&1`
 
 #sudo mkdir /home/vagrant/bin
 #chown -R /home/vagrant/bin vagrant
